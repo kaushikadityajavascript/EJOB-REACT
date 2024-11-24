4c02b7d79d50984a3226a7a4679cf6778638ff17
kaushikadityajavascript <kaushikaditya90@gmail.com>
2024-11-24 23:01:37 +0530

    card-layout

--- src/components/cards/Child.tsx
+++ src/components/cards/Child.tsx
@@ -14,13 +14,13 @@ const CardDetails: React.FC<CardProps> = ({ card }) => {
     }
 
     return (
-        <div className="card">
+        <div className="card details-card">
             <div className="card-body">
                 <h1 className="card-title">Card Details</h1>
             <p className="card-text"><strong>Id:</strong>{card.id}</p>
             <p className="card-text"><strong>Name:</strong>{card.name}</p>
                 <p className="card-text"><strong>Location:</strong>{card.loc}</p>
-                <img src={card.image} className="img-fluid rounded" />
+                <img src={card.image} className="img-fluid rounded details-image" />
             </div>
             
         </div>
